2ca94a27817129856877fac3808a717d8d06c2b0
kanwal-13 <99640319+kanwal-13@users.noreply.github.com>
2022-07-27 15:55:03 +0530

    removed id

--- django_email_tracker/settings.py
+++ django_email_tracker/settings.py
@@ -129,5 +129,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'kanwal130803@gmail.com'
-EMAIL_HOST_PASSWORD = 'aitdgszwwsdqczxz'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
